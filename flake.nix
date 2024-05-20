249b25f7d346146d10e91ce9305018d2b19db649
Faer-Anne <nina@projectmakeit.com>
2024-05-20 18:25:21 -0400

    test commit

---
+++ flake.nix
@@ -0,0 +1,26 @@
+{
+  inputs = {
+    fenix = {
+      url = "github:nix-community/fenix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    nixpkgs.url = "nixpkgs/nixos-23.11";
+    utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, utils, fenix }:
+    utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            #f.stable.toolchain
+            nodejs_21
+            wasm-pack
+          ];
+        };
+      }
+    );
+}
